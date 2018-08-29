958fe8b8688e63bbcd4ef849cb08c94ca3753a6c
chenzhiyuan <chenzy@innosilicon.com.cn>
2018-08-29 11:18:46 +0800

    add socket timeout

--- Service/SocketFunc.php
+++ Service/SocketFunc.php
@@ -11,6 +11,8 @@ class SocketFunc
         try {
             $socket = null;
             $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+            socket_set_option($socket,SOL_SOCKET,SO_RCVTIMEO,array("sec"=>10, "usec"=>0 ) );
+            socket_set_option($socket,SOL_SOCKET,SO_SNDTIMEO,array("sec"=>10, "usec"=>0 ) );
             if ($socket === false || $socket === null) {
                 $error = socket_strerror(socket_last_error());
                 return null;
